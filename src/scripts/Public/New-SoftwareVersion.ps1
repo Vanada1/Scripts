a3293ba4471e505ad76c012bf1475b6875e18d06
vladimir.shvoev <vladimir.shvoev@50ohm.tech>
2024-11-26 17:27:44 +0700

    feature(New-SoftwareVersion.ps1): добавлен шаблон для создания скрипта составления новой версии ПО

---
+++ src/scripts/Public/New-SoftwareVersion.ps1
@@ -0,0 +1,58 @@
+Function New-SoftwareVersion {
+    [CmdletBinding()]
+    [OutputType()]
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$LastVersion,
+
+        [Parameter(Mandatory = $true)]
+        [string]$IncrementVersionType,
+        
+        # TODO: Возможно, вместо использования только релиза кандидата, использовать более гибкую структуру,
+        # чтобы можно было работать с другими вариантами версий (например, alpha или beta релизы)
+        [Parameter(Mandatory = $false)]
+        [switch]$ReleaseCandidate,
+
+        [Parameter(Mandatory = $false)]
+        [string]$ReleaseCandidateLastVersion,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$Build,
+        
+        [Parameter(Mandatory = $false)]
+        [string]$BuildVersion
+    )
+
+    $availableIncrementVersionType = @(
+        'Major', 'Minor', 'Patch', 'ReleaseCandidate', 'M', 'm', 'p', 'rc')
+
+    if (-not $availableIncrementVersionType.Contains($IncrementVersionType)) {
+        Write-Error "$IncrementVersionType is not available version type. You must use the following available types: $availableIncrementVersionType" -ErrorAction Stop
+    }
+
+    $regexString = '^(?<major>\d+)\.(?<minor>\d+).(?<patch>\d+)+(?<pre_release>(\-\w+\.\d+)?)(\+(?<build>\d+))?$'
+    $newVersion = ''
+
+    if ($IncrementVersionType -eq 'Major' -or $IncrementVersionType -eq 'M') {
+
+    }
+    
+    if ($IncrementVersionType -eq 'Minor' -or $IncrementVersionType -eq 'm') {
+        
+    }
+    
+    if ($IncrementVersionType -eq 'Patch' -or $IncrementVersionType -eq 'p') {
+        
+    }
+
+    if ($ReleaseCandidate -and ($IncrementVersionType -eq 'ReleaseCandidate' -or $IncrementVersionType -eq 'rc')) {
+        
+    }
+
+    if ($Build) {
+        $newVersion += "+$BuildVersion"
+    }
+
+    $newVersion
+}
